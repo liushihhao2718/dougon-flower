dbae2f983ef09730de4a7b3bb568e41775729da8
劉士豪 <st930112@gmail.com>
2017-02-14 13:21:48 +0800

    fit curve 畫上後，polyline會刪除

--- src/Controls/PaintControl.js
+++ src/Controls/PaintControl.js
@@ -18,27 +18,40 @@ function PaintControl(pannel) {
 
 	this.end = function() {
 		let smoothBizer = fitCurve( rawPointData, error );
-		let pathString = fittedCurveDataToPathString(smoothBizer);
-		pannel.path( pathString ).fill('none').stroke({ width: 3 }).stroke('#f06');
-		rawPointData.length = 0;
+		let pathString = fittedCurveToPathString(smoothBizer);
+
+		// draw magnetic curve
+		drawOnPannel(pannel, pathString);
+		clearRawData();
 	};
+
+	function updateLines(paintingPolyLine, rawPointData) {
+		paintingPolyLine.plot( rawPointData );
+	}
+	function fittedCurveToPathString(fittedLineData) {
+		var str = '';
+		//bezier : [ [c0], [c1], [c2], [c3] ]
+		fittedLineData.map(function (bezier, i) {
+			if (i == 0) {
+				str += 'M ' + bezier[0][0] + ' ' + bezier[0][1];
+			}
+
+			str += 'C ' + bezier[1][0] + ' ' + bezier[1][1] + ', ' +
+			bezier[2][0] + ' ' + bezier[2][1] + ', ' +
+			bezier[3][0] + ' ' + bezier[3][1] + ' ';	
+						
+		});
+
+		return str;
+	}
+	function drawOnPannel(pannel, pathString){
+		pannel.path( pathString ).fill('none').stroke({ width: 3 }).stroke('#f06');
+	}
+	function clearRawData(){
+		rawPointData.length = 0;
+		paintingPolyLine.remove();
+	}
 }
 
 export default PaintControl;
 
-function updateLines(paintingPolyLine, rawPointData) {
-	paintingPolyLine.plot( rawPointData );
-}
-function fittedCurveDataToPathString(fittedLineData) {
-	var str = '';
-	fittedLineData.map(function (bezier, i) {
-		if (i == 0) {
-			str += 'M ' + bezier[0][0] + ' ' + bezier[0][1];
-		}
-		str += 'C ' + bezier[1][0] + ' ' + bezier[1][1] + ', ' +
-            bezier[2][0] + ' ' + bezier[2][1] + ', ' +
-            bezier[3][0] + ' ' + bezier[3][1] + ' ';
-	});
-
-	return str;
-}
